78967143e79301b1132f9bf169d0466a59015d01
Tom Nguyen <tndallas9517@gmail.com>
2023-03-10 13:25:31 -0600

    230310 worked on Index and Companies pages

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SplashpageTiny.Pages
+{
+    public class AboutcshtmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Companies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SplashpageTiny.Pages
+{
+    public class CompaniesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
